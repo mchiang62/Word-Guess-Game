406e1bab080cd49397d81a9a598f2d757965e0b0
Melissa Chiang <mchiang62@gmail.com>
2020-09-30 21:10:37 -0400

    updating code to ES6 v1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordList = [
+const wordList = [
     "broccoli",
     "asparagus",
     "lettuce",
@@ -12,71 +12,70 @@ var wordList = [
     "kale",
 ];
 
-// words selected - random
-var selectedWord = "";
+// to generate random words
+let selectedWord = "";
 
-// need var to split the letters in the word
-var lettersinWord = [];
+// split the letters in the word
+let lettersinWord = [];
 
 //number of blanks
-var numofBlanks = 0;
+let numofBlanks = 0;
 
 // b r _ _ c c_ _ _ 
-var blanksAndSuccesses = []; 
+let blanksAndSuccesses = [];
 
 // Wrong letters guessed
-var wrongGuesses = [];
+let wrongGuesses = [];
 
 // Game counts
-var wins = 0;
-var losses = 0;
-var guessesLeft = 7;
+let wins = 0;
+let losses = 0;
+let guessesLeft = 7;
 
 // create variables  - document.ElementbyId
 
-var directions = document.getElementById("directionstext");
-var remainder = document.getElementById("guessesRemaining");
-var guessed = document.getElementById("guessedLetters");
-var win = document.getElementById("winCount");
-var loss = document.getElementById("lossCount");
-var guesses = document.getElementById("guesses");
+let directions = document.getElementById("directionstext");
+let remainder = document.getElementById("guessesRemaining");
+let guessed = document.getElementById("guessedLetters");
+let win = document.getElementById("winCount");
+let loss = document.getElementById("lossCount");
+let guesses = document.getElementById("guesses");
 
 
 
 
 // FUNCTIONS - this starts the game
 
-
-function startGame () {
+const startGame = () => {
     selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
     lettersinWord = selectedWord.split("");
     numofBlanks = lettersinWord.length;
 
-    // splits words into an array .split
+    // splits words into an array using array.split
 
     // create a reset - need to reset or it won't work
     guessesleft = 7;
     wrongGuesses = [];
     blanksAndSuccesses = [];
 
-    
+
     // need to create loop to populate blanks and successes
 
     for (var i = 0; i < numofBlanks; i++) {
-     blanksAndSuccesses.push("_");
+        blanksAndSuccesses.push("_");
 
     }
 
     console.log(blanksAndSuccesses);
 
-// linking js text to HTML
+    // linking js text to HTML
 
-   guesses.textContent = blanksAndSuccesses.join("  ");
-   remainder.textContent = guessesLeft;
-   win.textContent = wins;
-   loss.textContent = losses;
+    guesses.textContent = blanksAndSuccesses.join("  ");
+    remainder.textContent = guessesLeft;
+    win.textContent = wins;
+    loss.textContent = losses;
 
-   
+
     // DEBUG & TEST:
     console.log(selectedWord);
     console.log(lettersinWord);
@@ -87,92 +86,90 @@ function startGame () {
 
 
 
-function checkLetters (letter) {
+function checkLetters(letter) {
 
 
 
     var LetterInWord = false;
-    for (var i = 0; i<numofBlanks; i++) {
-    // console.log(selectedWord[i]);
+    for (var i = 0; i < numofBlanks; i++) {
+        // console.log(selectedWord[i]);
         if (selectedWord[i] == letter) {
             LetterInWord = true;
 
         }
     }
 
+    // create for loop to check existing letters - blanksAndSuccesses 
+    if (LetterInWord) {
+        for (var i = 0; i < numofBlanks; i++) {
+            if (selectedWord[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+            }
+        }
+    } else {
+        wrongGuesses.push(letter);
+        guessesLeft--
 
-// create for loop to check existing letters - blanksAndSuccesses 
-if(LetterInWord) {
-    for (var i=0; i<numofBlanks; i++) {
-    if(selectedWord[i] == letter) {
-        blanksAndSuccesses[i] = letter;
-     }
     }
-}
 
-
-
-else {
-    wrongGuesses.push(letter);
-    guessesLeft--
+    console.log(blanksAndSuccesses);
 
 }
 
-console.log(blanksAndSuccesses);
-
-}
-
-function roundComplete(){
+function roundComplete() {
     console.log("Win Count: " + wins + " | Loss Count: " + losses + " | Guesses Remaining " + guessesLeft);
 
-guessesRemaining.textContent = guessesLeft;
-guesses.textContent = blanksAndSuccesses.join(" ");
-guessed.textContent = wrongGuesses.join("  ");
+    guessesRemaining.textContent = guessesLeft;
+    guesses.textContent = blanksAndSuccesses.join(" ");
+    guessed.textContent = wrongGuesses.join("  ");
 
 
 
     if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
         wins++;
         alert("YOU WIN :)");
-        
-win.textContent = wins;
 
-startGame();
+        win.textContent = wins;
 
+        startGame();
+
+
+    } else if (guessesLeft == 0) {
+        losses++;
+        alert("YOU LOSE :(");
+
+        loss.textContent = losses;
+
+        startGame();
+
+        location.reload();
+
+        // put restart game function?
 
     }
 
-else if (guessesLeft == 0) {
-    losses++;
-    alert("YOU LOSE :(");
-
-    loss.textContent = losses;
-
-    startGame(); 
-
-    location.reload();
-
-    // put restart game function?
-
 }
 
-}
 
-// MAIN PROCESS in order for the function to work:
-startGame ();
+startGame();
 
 // keyclicks
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-checkLetters(letterGuessed);
-roundComplete();
+    checkLetters(letterGuessed);
+    roundComplete();
 
-directions.textContent = "";
+    directions.textContent = "";
 
-  console.log(letterGuessed);
+    console.log(letterGuessed);
 
 
 }
+
+
+//things to still work on
+//when the user loses, it shouldn't restart - it should keep going 
+
 
 
 
